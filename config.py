98ed8698fe8d46e5e33a9d9c2e2921538c299002
ymmmmmmmm <51306299+ymmmmmmmm@users.noreply.github.com>
2024-02-06 17:20:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time     :2024/2/6 17:03
+# @Author   :ym
+# @File     :config.py
+# @Software :PyCharm
+# yescaptcha key
+yescaptcha_client_key = 'cc9c18d3e263515c2c072b36a7125eecc078618f'
+# 代理提取链接
+get_ip_url = 'https://www.ip.com'
